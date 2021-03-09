8dd71ff029b6725654e40e7df4d47acb0fcc3ba3
Anna <buettner.anna@googlemail.com>
2021-03-09 14:42:55 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.15

---
+++ src/AmbientForcing.jl
@@ -0,0 +1,5 @@
+module AmbientForcing
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AmbientForcing
+using Test
+
+@testset "AmbientForcing.jl" begin
+    # Write your tests here.
+end
