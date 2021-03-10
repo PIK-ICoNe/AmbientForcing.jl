1567c137512286f73f336ac0b82263138feb0f16
Anna <buettner.anna@googlemail.com>
2021-03-10 14:23:05 +0100

    Added Random Vector Functions

--- src/AmbientForcingODE.jl
+++ src/AmbientForcingODE.jl
@@ -1,13 +1,9 @@
 """
-Calculates and returns the constraint equations from an ODEFunction used in
-DifferentialEquations.jl.
-The system must be in Mass Matrix form meaning: M ẋ = f(x). The constraints can
-therefore be easily extracted by finding the diagonal entries of M which are 0.
-Inputs:
-       f: An ODEFunction in mass matrix form
-Outputs:
-        g(x): A function which holds all the constraints of f.
+    constraint_equations(f::ODEFunction)
 
+Returns the constraint equations from an ODEFunction f used in DifferentialEquations.jl.
+
+f must be in Mass Matrix form meaning: M ẋ = f(x).
 """
 function constraint_equations(f::ODEFunction)
     M = f.mass_matrix
@@ -19,53 +15,51 @@ function constraint_equations(f::ODEFunction)
 end
 
 """
-In DAE Problems not all initial conditions are valid. They lay on a constraint Manifold M.
-This method can draw random initial condition form the constraint manifold M.
-Here we sample values by using the AmbientForcing algorithm.
+    AmbientForcing(f::ODEFunction, z, tau, Frand)
+Draws random initial condition form the constraint manifold M.
+
 This consists of the following steps:
     1. Draw a random vector Frand from the ambient space
     2. Projetion back onto the tangetial space TxM by using PN
 
-Inputs:
+# Arguments
     f: An ODEFunction in mass matrix form
     initial_con: A point which lays on the manifold M, could e.g. be a fixed point
     dist_args: Arguments of the distribution function dist
     dist: Distribution function e.g. Uniform, Normal....
     tau: Integration time
-
-Outputs:
-    sol: Set of random initial condtions on M
 """
 function AmbientForcing(f::ODEFunction, z, tau, Frand)
     g = constraint_equations(f)
     prob = ODEProblem(ambient_forcing_ODE, z, (0.0, tau), (g, Frand))
     sol = solve(prob)
-    return sol
+    return sol[end]
 end
 
 """
-Calculates the orthogonal projection on a subspace N. The basis of N does not
-have to be a orthonormal basis. The matrix (A^T*A)^-1 recovers the norm.
-Inputs:
-A: Matrix containg the basis vectors of a subspace N as columns
+    Proj_N(A)
+
+Calculates the orthogonal projection matrix on a subspace N.
+
+A is a matrix containg the basis vectors of N as columns.
+The matrix (A^T*A)^-1 recovers the norm.
 """
 Proj_N(A) = A * inv(transpose(A) * A) * transpose(A)
 
 """
-Takes a constraint function g and a random value Frand form the ambient space 
-and calculates the projection on to the tangetial space. This gives a manifold
-preserving version of any dynamic.
-ż = Pn(ker(Jg)) * Frand
-This is an ODE which will be solved using DifferentialEquations.jl
-Inputs:
-p[1] = g: The function defining the manifold
-p[2] = Frand: A random, constant vector from ambient space
-u0: Inital condition
-t: Integration time
+    ambient_forcing_ODE(u, p, t)
+The manifold preserving ODE version of a vector h.
+
+ż = Pn(ker(Jg)) * h
+# Arguments
+    `p[1]`: g the function defining the manifold
+    `p[2]` h constant vector from ambient space
+    `u0`: Inital condition
+    `t`: Integration time
 """
 function ambient_forcing_ODE(u, p, t)
-    g, Frand = p
+    g, h = p
     Jacg = ForwardDiff.jacobian(g, u)
     N = nullspace(Jacg)
-    du = Proj_N(N) * Frand # Projection back onto the Manifold
+    du = Proj_N(N) * h # Projection back onto the Manifold
 end
--- src/RandInitVectors.jl
+++ src/RandInitVectors.jl
@@ -1,79 +1,89 @@
-function RandomForce(f::ODEFunction, dist_args, 
-    dist::UnionAll, node::Int64, tau; method = "Voltage")
+"""
+    idx_exclusive(f::ODEFunction, str::Array{String})
+"""
+function idx_exclusive(f::ODEFunction, str::Array{String})
+    idx_vec = Array{Int64}(undef, length(str))
+   
+    for j in 1:length(str)
+        for (i, s) in enumerate(f.syms)
+            if string(str[j]) == string(s)
+                idx_vec[j] = i
+            end
+        end
+    end
+    return idx_vec
+end
+
+"""
+    RandomForce(f::ODEFunction, dist_args, dist::UnionAll, idx_vec::Array{Array{Float64}})
+
+Perturbes only the variables in given in idx_vec. Each variable is initalized using its own distribution.
+"""
+function RandomForce(f::ODEFunction, dist_args::Array{Array{Float64, 1}, 1}, 
+                    dist::UnionAll, idx_vec)
+    if length(idx_vec) != length(dist_args)
+        error("Please give dist_args for each var you want to perturb.")
+    end
     M = f.mass_matrix
     len_M = size(M, 1)
     Frand = zeros(len_M)
-    if method == "Voltage" # Perturbs only the voltage at a node
-        node_vars = NodeToVarIdxVolt(f, node)
-        Frand[node_vars] = rand(dist(dist_args...), length(node_vars))
-    elseif  method == "All" # Perturbs all variables at a node
-        node_vars = NodeToVarIdx(f, node)
-        Frand[node_vars[1:2]] = rand(dist(dist_args...), 2) # Perturbes the voltages
-        if length(node_vars) > 2
-            Frand[node_vars[3]] = rand(dist(0, 2π/tau)) # Perturbes the angle θ
-            Frand[node_vars[4]] = rand(dist(-100/tau, 100/tau)) # Perturbes the angular velocity ω
-        end
+    for i in 1:length(idx_vec)
+        Frand[idx_vec[i]] = rand(dist(dist_args[i]...))
     end
     return Frand
 end
 
-function RandomForce(dist_args, dist::UnionAll, len_M::Int64)
-Frand = rand(dist(dist_args...), len_M)
-end
+"""
+    RandomForce(f::ODEFunction, dist_args::Array{Float64}, dist::UnionAll, idx_vec)
 
+Perturbes only the variables in given in idx_vec.
 """
-Takes a ODEFunction from PowerDynamics and returns the indexes of the dynamical
-variables associated to a node.
-Inputs:
-f: The rigthhand side of a PowerGrid object
-node: Variable indexes of this node are found
-Outputs:
-vars_array: Array of the indexes of the variables of the node
-"""
-function NodeToVarIdx(f::ODEFunction, node::Int64)
+function RandomForce(f::ODEFunction, dist_args::Array{Float64}, 
+                    dist::UnionAll, idx_vec)
     M = f.mass_matrix
     len_M = size(M, 1)
-    var_array = []
+    Frand = zeros(len_M)
 
-    for i in 1:len_M
-        str = string(f.syms[i])
-        idx = findlast('_', str)
-        node_num = parse(Int64, str[idx + 1:end])
-
-        if node_num > node
-            return var_array
-        end
-
-        if node_num == node
-            append!(var_array, i)
-        end
-    end
-    return var_array
+    Frand[idx_vec] = rand(dist(dist_args...), length(idx_vec))
+    return Frand
 end
 
+
 """
-Takes a ODEFunction from PowerDynamics and returns the indexes of the voltages
-associated to a node.
-Inputs:
-f: The rigthhand side of a PowerGrid object
-node: Variable indexes of this node are found
-Outputs:
-vars_array: Array of the indexes of the voltages of the node
+    RandomForce(f::ODEFunction, dist_args, dist::UnionAll)
+Perturbes all variables.
 """
-function NodeToVarIdxVolt(f::ODEFunction, node::Int64)
+function RandomForce(f::ODEFunction, dist_args::Array{Float64}, dist::UnionAll)
     M = f.mass_matrix
     len_M = size(M, 1)
-    var_array = []
+    Frand = rand(dist(dist_args...), len_M)
+end
 
-    ur = "u_r" * "_" * string(node)
-    ui = "u_i" * "_" * string(node)
+function voltage_str(node::Int)
+    voltage_str = ["u_r_" * string(node), "u_i_" * string(node)]
+end
 
-    for i in 1:len_M
-        if string(f.syms[i]) == ui
-            append!(var_array, i)
-        elseif string(f.syms[i]) == ur
-        append!(var_array, i)
+function full_str(pg::PowerGrid, node::Int)
+    if length(symbolsof(pg.nodes[node])) == 2
+        full_str = ["u_r_" * string(node), "u_i_" * string(node)]
+    elseif :θ ∈ symbolsof(pg.nodes[node])
+        if :ω ∈ symbolsof(pg.nodes[node])
+            full_str = ["u_r_" * string(node), "u_i_" * string(node), "θ_" * string(node), "ω_" * string(node)]
+        else
+            error("Please constrcut the vector manually")
         end
+    else
+        error("Please constrcut the vector manually")
     end
-    return var_array
+end
+
+function pd_node_idx(pg::PowerGrid, node::Int, method::String)
+    if method == "Voltage"
+        str_vec = voltage_str(node)
+    elseif method == "All"
+        str_vec = full_str(pg, node)
+    else
+        error("Please use a valid method.")
+    end
+    idx_vec =  idx_exclusive(rhs(pg), str_vec)
 end
