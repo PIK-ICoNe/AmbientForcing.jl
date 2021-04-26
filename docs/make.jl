d0cfbbfdffab735384aa2fb098863feae7e4580b
Anna <buettner.anna@googlemail.com>
2021-04-26 09:58:00 +0200

    docs test

--- docs/make.jl
+++ docs/make.jl
@@ -1,33 +1,46 @@
 using Documenter, AmbientForcing
 using Literate
 
+
 # generate examples
-my_path = joinpath(@__DIR__, "..", "examples")
+examples = [
+    joinpath(@__DIR__, "..", "examples", "DifferentialEquation_example.jl"),
+    # joinpath(@__DIR__, "..", "examples", "kuramoto_network.jl"),
+    #joinpath(@__DIR__, "..", "examples", "kuramoto_without_nd.jl"),
+    #joinpath(@__DIR__, "..", "examples", "pd_node.jl"),
+]
 
 OUTPUT = joinpath(@__DIR__, "src/generated")
 isdir(OUTPUT) && rm(OUTPUT, recursive=true)
 mkpath(OUTPUT)
 
-Literate.markdown(joinpath(my_path, "DifferentialEquation_example.jl"), OUTPUT)
-Literate.script(joinpath(my_path, "DifferentialEquation_example.jl"), OUTPUT)
+for ex in examples
+    Literate.markdown(ex, OUTPUT)
+    Literate.script(ex, OUTPUT)
+end
 
-#Literate.markdown(joinpath(path, "NetworkDynamics_example.jl"), OUTPUT)
-#Literate.markdown(joinpath(path, "PowerDynamics_example.jl"), OUTPUT)
-
-makedocs(sitename = "AmbientForcing.jl",
-        modules = [AmbientForcing],
-        authors = "Anna Büttner and contributors",
-        format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-        pages = [
+makedocs(;
+    modules = [AmbientForcing],
+    authors = "Anna Büttner and contributors",
+    sitename = "AmbientForcing.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://github.com/Anbue63/AmbientForcing.jl",
+        assets = String[],
+    ),
+    pages = [
         "Home" => "index.md",
         "Examples" => ["OrdinaryDiffEq" => "generated/DifferentialEquation_example.md",
-                       #"PowerDynamics" => "generated/PowerDynamics_example.md",
-                       #"NetworkDynamics" => "generated/NetworkDynamics_example.md"
-                       ]
+                       #"PowerDynamics.jl Node" => "generated/pd_node.md",
+                       # "Kuramoto Network" => "generated/kuramoto_network.md",
+                       #"Kuramoto without ND.jl" => "generated/kuramoto_without_nd.md"
+                    ]
     ],
 )
 
+
 deploydocs(;
-    repo = "github.com/Anbue63/AmbientForcing.jl",
-    devbranch = "master",
+    repo="https://github.com/Anbue63/AmbientForcing.jl",
+    devbranch="master",
+    # push_preview=true,
 )
---
+++ docs/src/index.md
@@ -0,0 +1,40 @@
+# AmbientForcing.jl Documentation
+
+```@meta
+CurrentModule = AmbientForcing
+```
+
+## Introduction
+Differential algebraic equations (DAEs) consist of differential equations $f$ and algebraic equations, or constraints $g$.
+
+```math
+\begin{align}
+        \dot{x} &= f(t, x(t),y(t))\\ 
+        0 &= g(t, x(t), y(t)) = g(z)
+    \end{align}
+```
+To solve these equations the problem is separated into two parts: finding valid initial conditions $(x_0,y_0,t_0)$ and calculating the trajectories .
+Different DAE initialization methods have been implemented and are used in various differential equation solvers. 
+
+Established initialization methods, like Brown's method, which is also included in \texttt{DifferentialEquations.jl}, can not be used to initalize localized Pertubations, needed for Single Node Basin Stability, since only a part of the system is initialized, and it is not possible to regulate what happens to the other variables during the initialization process. Thus it can not be controlled if a perturbation is only targeted to a single node.
+This package offers an novel algorithm, called Ambient Forcing, to calulate valid localized iital conditions for DAE systems.
+
+The center of the alogirtihm is solving the following differential equation, to generate new initial states. 
+It is based on a theorem form Differentail Geometry that holds for smooth manifolds. 
+
+Where $\dot z = h(z)$ is an arbitrary dynamic  the manifold preserving version can simply defined as:
+
+For an arbitrary dynamic $\dot z = h(z)$ the manifold preserving version can simply defined as:
+
+```math
+    \begin{align}
+        \dot z = P^\mathcal{N}(z) h(z)
+    \end{align}
+```
+Where $P^\mathcal{N}(z)$ is a projection matrix.
+
+```@docs
+ambient_forcing
+```
+
+Generally the constraint manifolds of power grids, which are given by the algebraic loads, are smooth.
