e0d8014ae36f40b643c3b473de2bde6fe5dc59d9
Anna <buettner.anna@googlemail.com>
2021-06-23 12:48:44 +0200

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,48 @@
 using Documenter
+using Literate
 using AmbientForcing
+using NetworkDynamics
+using PowerDynamics
+using Distributions
+using LightGraphs
 
-makedocs(
-    sitename = "AmbientForcing",
-    format = Documenter.HTML(),
-    modules = [AmbientForcing]
+
+# generate examples
+examples = [
+    joinpath(@__DIR__, "..", "examples", "DifferentialEquation_example.jl"),
+    joinpath(@__DIR__, "..", "examples", "NetworkDynamics_example.jl"),
+    joinpath(@__DIR__, "..", "examples", "PowerDynamics_example.jl"),
+]
+
+OUTPUT = joinpath(@__DIR__, "src/generated")
+isdir(OUTPUT) && rm(OUTPUT, recursive=true)
+mkpath(OUTPUT)
+
+for ex in examples
+    Literate.markdown(ex, OUTPUT)
+    Literate.script(ex, OUTPUT)
+end
+
+makedocs(;
+    modules=[AmbientForcing],
+    authors="Anna Büttner and contributors",
+    repo="https://github.com/Anbue63/AmbientForcing.jl",
+    sitename="AmbientForcing.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://github.com/Anbue63/AmbientForcing.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Examples" => ["DiffEq" => "generated/DifferentialEquation_example.md",
+                       "NetworkDynamics" => "generated/NetworkDynamics_example.md",
+                       "PowerDynamics" => "generated/PowerDynamics_example.md"]
+    ],
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(;
+    repo="github.com/Anbue63/AmbientForcing.jl",
+    devbranch="master",
+    # push_preview=true,
+)
